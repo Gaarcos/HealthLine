54fd7106f9945b6e2a879e4f4aca5e26030aa910
Gaarcos <gabrielsuehiro@gmail.com>
2023-03-26 02:04:36 -0300

    PriorityQueue done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const Queue = require('./queue');
-
+const PriorityQueue = require('./priorityQueue');
 
 const app = express();
 const secretKey = 'minha-chave-secreta';
@@ -151,6 +151,72 @@ app.get('/queue/elements', (req, res) => {
   const elements = queue.toArray();
 
   res.send(`Elementos na fila: ${elements.map(e => `${e.name}`).join(', ')}`);
+});
+
+const priorityQueue = new PriorityQueue();
+
+// Adiciona um elemento na fila prioritária
+app.post('/priorityQueue/enqueue', (req, res) => {
+  const name = req.query.name;
+  const doenca = req.query.doenca;
+  if(doenca != 'morrendo'){
+    res.status(800).send('você deve entrar na fila normal, pois não atende aos requisitos de prioridade.')
+  }
+  if (!name) {
+    res.status(400).send('O parâmetros "name" obrigatórios.');
+  } else {
+    priorityQueue.enqueue({ name });
+    res.send(`Elemento com nome ${name} adicionado à fila prioritária.`);
+  }
+});
+
+
+// Remove um elemento da fila prioritária
+app.delete('/priorityQueue/dequeue', (req, res) => {
+  const name = req.query.name;
+  if (priorityQueue.isEmpty()) {
+    res.status(400).send('A fila está vazia.');
+  } else {
+    priorityQueue.dequeue();
+
+    res.send(`Elemento removido da fila prioritária: ${name}`);
+  }
+});
+
+// Retorna o tamanho da fila prioritária
+app.get('/priorityQueue/size', (req, res) => {
+  const size = priorityQueue.size();
+
+  res.send(`Tamanho da fila prioritária: ${size}`);
+});
+
+// Retorna a posição de um elemento na fila prioritária
+app.get('/priorityQueue/position', (req, res) => {
+  const elementName = req.query.name;
+
+  if (!elementName) {
+    res.status(400).send('O parâmetro "name" é obrigatório.');
+  } else {
+    const position = priorityQueue.indexOf(elementName);
+
+    if (position === -1) {
+      res.send(`O elemento ${elementName} não está na fila prioritária.`);
+    }
+    else if(position=== 0){
+      res.send(`O elemento ${elementName} é o proximo a ser chamado na fila prioritária`);
+    }
+    else {
+      res.send(`O elemento ${elementName} está na posição ${position + 1} da fila prioritária.`);
+    }
+  }
+});
+
+
+// Retorna todos os elementos na fila prioritária
+app.get('/priorityQueue/elements', (req, res) => {
+  const elements = priorityQueue.toArray();
+
+  res.send(`Elementos na fila prioritária: ${elements.map(e => `${e.name}`).join(', ')}`);
 });
 
 // Inicia o servidor na porta 3000
