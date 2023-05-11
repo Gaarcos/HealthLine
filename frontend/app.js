5d953500e8e94b9a46b425bf6a1592af18a013ae
Gaarcos <gabrielsuehiro@gmail.com>
2023-05-11 10:46:32 -0300

    changes

--- frontend/app.js
+++ frontend/app.js
@@ -1,8 +1,11 @@
+const express = require('express');
+const app = express();
+
 app.use(express.static(path.join(__dirname, 'controllers')));
 
 app.get('/style.css', (req, res) => {
   res.type('text/css');
-  res.sendFile(path.join(__dirname, '../views/style.css'));
+  res.sendFile(path.join(__dirname, '../public/styles/style.css'));
 });
 // Inicia o servidor na porta 3000
 app.listen(3000, () => {
