4eecce9bbdba1f589efd12d05514fe60ac9ef47c
Gaarcos <gabrielsuehiro@gmail.com>
2023-05-12 00:40:05 -0300

    css funcional

--- app.js
+++ app.js
@@ -6,29 +6,30 @@ const authenticateMiddleware = require("./config/middleware");
 
 const app = express();
 
-const viewsPath = path.join(__dirname, './views');
-const imagesPath = path.join(__dirname, './public/imagens');
-const stylesPath = path.join(__dirname, './public/styles');
+const viewsPath = path.join(__dirname, '/views');
+const imagesPath = path.join(__dirname, '/public/imagens');
+const stylesPath = path.join(__dirname, '/public/styles');
 
 app.use(express.json());
+app.use(express.static(stylesPath));
 app.use(express.static(viewsPath));
 app.use('/imagens', express.static(imagesPath));
 
 app.get('/style.css', (req, res) => {
-res.type('text/css');
-res.sendFile(path.join(stylesPath, 'styles.css'));
+    res.type('text/css');
+    res.sendFile(path.join(stylesPath, 'styles.css'));
 });
 
 app.get('/cadastro', (req, res) => {
-res.sendFile(path.join(viewsPath, 'cadastro.html'));
+    res.sendFile(path.join(viewsPath, 'cadastro.html'));
 });
 
 app.get('/home', (req, res) => {
-res.sendFile(path.join(viewsPath, 'home.html'));
+    res.sendFile(path.join(viewsPath, 'home.html'));
 });
 
 app.get('/login', (req, res) => {
-res.sendFile(path.join(viewsPath, 'login.html'));
+    res.sendFile(path.join(viewsPath, 'login.html'));
 });
 
 app.use("/auth", AuthController);
